3835047e83393aafe9347dc658e85a5e3bcd3af3
Sam Pilgrim <ramsom11@gmail.com>
2020-03-24 09:55:36 +1000

    Stopped arrow keys from scrolling page

--- control.js
+++ control.js
@@ -1,6 +1,12 @@
 // Code © 2020 Charlie McMahon. All Rights Reserved.
 //this caused me so much pain
 var fired = false;
+window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
 
 document.onkeydown = function(event) {
     if(!fired) {
