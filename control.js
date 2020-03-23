73838f8cca0bfe5f569f1614e315d83a040f9d66
Charlie McMahon <51872183+charliekmcmahon@users.noreply.github.com>
2020-03-23 15:59:09 +1000

    Update control.js

--- control.js
+++ control.js
@@ -1,73 +1,20 @@
-document.onkeydown = checkKey;
-abc = 1;
+var Key = {
+  _pressed: {},
 
-function checkKey(e) {
+  LEFT: 37,
+  UP: 38,
+  RIGHT: 39,
+  DOWN: 40,
 
-    e = e || window.event;
+  isDown: function(keyCode) {
+    return this._pressed[keyCode];
+  },
 
-    if (e.keyCode == '38') {
-        up();
-        checkKeyUp();
-    }
-    else if (e.keyCode == '40') {
-        down();
-        checkKeyUp();
-    }
-    else if (e.keyCode == '37') {
-       left();
-       checkKeyUp();
-    }
-    else if (e.keyCode == '39') {
-       right();
-       checkKeyUp();
-    }
-    else if (e.keyCode == '32') {
-       drop();
-       checkKeyUp();
-    }
-    else if (e.keyCode == '83') {
-       swicherydoo();
-       checkKeyUp();
-    }
+  onKeydown: function(event) {
+    this._pressed[event.keyCode] = true;
+  },
 
-}
-document.onkeyup = checkKeyUp;
-
-function checkKeyUp(a) {
-
-    a = a || window.event;
-
-    if (a.keyCode == '38') {
-        stop();
-
-    }
-    else if (a.keyCode == '40') {
-        stop();
-
-    }
-    else if (a.keyCode == '37') {
-       stop();
-
-    }
-    else if (a.keyCode == '39') {
-       stop();
-
-    }
-    else if (a.keyCode == '83') {
-       stop();
-    }
-    else if (a.keyCode == '32') {
-       stop();
-    }
-    else if (a.keyCode == '83') {
-        stop();
-    }
-
-}
-
-while (abc = 1) {
-
-    checkKey();
-    checkKeyUp();
-
-}
+  onKeyup: function(event) {
+    delete this._pressed[event.keyCode];
+  }
+};
