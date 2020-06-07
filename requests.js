1d57d45b81928a7dad21fa7fbe13e90d3ce217e2
Sam Pilgrim <ramsom11@gmail.com>
2020-06-07 14:04:14 +1000

    Update requests.js
    changed ports

--- requests.js
+++ requests.js
@@ -1,35 +1,35 @@
-var mainCamera = "http://ramcraft.ddns.net:8081/video"
-var sideCamera = "http://ramcraft.ddns.net:8082/video"
+var mainCamera = "http://ramcraft.ddns.net:443/video"
+var sideCamera = "http://ramcraft.ddns.net:415/video"
 
 window.onload = function() {
   var right = new XMLHttpRequest();
-    right.open("GET","http://ramcraft.ddns.net:8080/start");
+    right.open("GET","http://ramcraft.ddns.net:80/start");
     right.send();
 }
 
 function up() {
     var up = new XMLHttpRequest();
-    up.open("GET","http://ramcraft.ddns.net:8080/up");
+    up.open("GET","http://ramcraft.ddns.net:80/up");
     up.send();
 }
 function down() {
     var down = new XMLHttpRequest();
-    down.open("GET","http://ramcraft.ddns.net:8080/down");
+    down.open("GET","http://ramcraft.ddns.net:80/down");
     down.send();
 }
 function left() {
     var left = new XMLHttpRequest();
-    left.open("GET","http://ramcraft.ddns.net:8080/left");
+    left.open("GET","http://ramcraft.ddns.net:80/left");
     left.send();
 }
 function right() {
     var right = new XMLHttpRequest();
-    right.open("GET","http://ramcraft.ddns.net:8080/right");
+    right.open("GET","http://ramcraft.ddns.net:80/right");
     right.send();
 }
 function stop() {
     var stop = new XMLHttpRequest();
-    stop.open("GET","http://ramcraft.ddns.net:8080/stop");
+    stop.open("GET","http://ramcraft.ddns.net:80/stop");
     stop.send();
 }
 
@@ -47,7 +47,7 @@ function swicherydoo(){
 }
 function drop() {
     var drop = new XMLHttpRequest();
-    drop.open("GET","http://ramcraft.ddns.net:8080/drop");
+    drop.open("GET","http://ramcraft.ddns.net:80/drop");
     drop.send();
 }
-var currentVid = "http://ramcraft.ddns.net:8082/video";
+var currentVid = "http://ramcraft.ddns.net:443/video";
