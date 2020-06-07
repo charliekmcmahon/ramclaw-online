3a7e56724876ae132cb6c8e3be11725c8bf279c9
Sam Pilgrim <ramsom11@gmail.com>
2020-06-07 14:23:49 +1000

    even more fix

--- requests.js
+++ requests.js
@@ -1,5 +1,5 @@
 var mainCamera = "http://ramcraft.ddns.net:443/video"
-var sideCamera = "http://ramcraft.ddns.net:465/video"
+var sideCamera = "http://ramcraft.ddns.net:25/video"
 
 window.onload = function() {
   var right = new XMLHttpRequest();
